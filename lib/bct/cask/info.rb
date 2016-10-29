4a3da42acc6d3e60e1f0d4eac7cdf2f202147b11
Dave Greene <davepgreene@gmail.com>
2016-10-29 14:45:48 -0400

    Made Info class namespacing consistent with directory structure.

--- lib/bct/cask/info.rb
+++ lib/bct/cask/info.rb
@@ -1,32 +1,34 @@
 module BrewCaskTools
-  # Class to keep brittle parsing of `brew cask info #{cask}` localized.
-  class Info
-    def initialize(arr)
-      @info = arr.nil? ? [] : arr
-    end
+  module Casks
+    # Class to keep brittle parsing of `brew cask info #{cask}` localized.
+    class Info
+      def initialize(arr)
+        @info = arr.nil? ? [] : arr
+      end
 
-    def short_name
-      @info[0]
-    end
+      def short_name
+        @info[0]
+      end
 
-    def url
-      @info[1]
-    end
+      def url
+        @info[1]
+      end
 
-    def path
-      @info[2]
-    end
+      def path
+        @info[2]
+      end
 
-    def cask_url
-      @info[3]
-    end
+      def cask_url
+        @info[3]
+      end
 
-    def name
-      @info[5]
-    end
+      def name
+        @info[5]
+      end
 
-    def app_name
-      @info[7]
+      def app_name
+        @info[7]
+      end
     end
   end
 end
